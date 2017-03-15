fa70fc971e512dbea8a728d795aee4bbbc4f339c
Daniel Bigelow <Interfer0@gmail.com>
2017-03-15 14:08:48 -0600

    Completed form and validation

--- src/Containers/FormContainer.js
+++ src/Containers/FormContainer.js
@@ -8,42 +8,121 @@ class FormContainer extends Component {
     this.handleClearForm = this.handleClearForm.bind(this);
     this.handleFullNameChange = this.handleFullNameChange.bind(this);
     this.handlePhoneChange = this.handlePhoneChange.bind(this);
+    this.handleWebChange = this.handleWebChange.bind(this);
+    this.handleEmailChange = this.handleEmailChange.bind(this);
   }
 
-  handlePhoneChange() {
-      if(document.getElementById('frmPhone').value.match(/[a-zA-Z]/)){
-            document.getElementById('frmPhone').style.color = 'red';
-      } else {
-        if(
-            (document.getElementById('frmPhone').value.match(/\d/g).length <= 10) ||
-            iz.blank(document.getElementById('frmPhone').value)
-        ) {
-            document.getElementById('frmPhone').style.color = 'black';
-        } else {
-            document.getElementById('frmPhone').style.color = 'red';
-        }
-      }
-      
-  }
+
 
   handleFullNameChange() {
-      if(
-          ( iz.string(document.getElementById('frmName').value) &&
-                !iz.number(document.getElementById('frmName').value) ) ||
-       iz.blank(document.getElementById('frmName').value)
-       ) {
-            document.getElementById('frmName').style.color = 'black';
+      var itm = document.getElementById('frmName');
+      if(itm.value.length >= 1 && itm.value.length != null)
+        itm.style.backgroundColor = 'white';
+      if(( iz.string(itm.value) && !iz.number(itm.value)) ||
+       iz.blank(itm.value)) 
+       {
+            itm.style.color = 'black';
+
        } else{
-           document.getElementById('frmName').style.color = 'red';
+           itm.style.color = 'red';
        }
   }
 
+  handlePhoneChange() {
+      var itm = document.getElementById('frmPhone');
+      if(itm.value.length >= 1 && itm.value.length != null)
+                itm.style.backgroundColor = 'white';
+      if(itm.value.match(/[a-zA-Z]/)){
+            itm.style.color = 'red';
+      } else {
+        if((itm.value.match(/\d/g).length <= 10) || iz.blank(itm.value)) 
+        {
+            itm.style.color = 'black';
+            
+        } else {
+            itm.style.color = 'red';
+        }
+      }
+  }
+    handleWebChange() {
+        var itm = document.getElementById('frmWeb');
+        if(itm.value.length >= 1 && itm.value.length != null)
+            itm.style.backgroundColor = 'white';
+            if(itm.value.length >= 1)
+            {
+                itm.style.color = 'black';
+                itm.style.backgroundColor = 'white';
+            }
+  }
+
+  handleEmailChange() {
+        var itm = document.getElementById('frmEmail');
+        if(itm.value.length >= 1 && itm.value.length != null)
+                    itm.style.backgroundColor = 'white';
+        if(itm.value.length >= 1)
+        {
+            itm.style.color = 'black';
+            itm.style.backgroundColor = 'white';
+        }
+  }
   handleFormSubmit() {
-    //setState({name : event.target.value});
-    console.log('here')
+      var gtg = true;
+        var itmN = document.getElementById('frmName');
+        var itmP = document.getElementById('frmPhone');
+        var itmW = document.getElementById('frmWeb');
+        var itmE = document.getElementById('frmEmail');
+        if((!iz.string(itmN.value) || iz.number(itmN.value)) || iz.blank(itmN.value))
+        {
+            gtg = false;
+            itmN.value = '';
+            itmN.placeholder = 'Invalid Name!';
+            itmN.style.backgroundColor = 'red';  
+        }
+        if((itmP.value.length > 10) || itmP.value.length < 7|| iz.blank(itmP.value) || itmP.value.match(/[a-zA-Z]/) != null)
+        {
+            gtg = false;
+            itmP.value = '';
+            itmP.placeholder = 'Invalid Phone Number!';
+            itmP.style.backgroundColor = 'red';  
+        }
+        if(iz.blank(itmW.value))
+        {
+            gtg = false;
+            itmW.value = '';
+            itmW.placeholder = 'Invalid Website!';
+            itmW.style.backgroundColor = 'red';  
+        }
+        var emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        if(!emailRegex.test(itmE.value)|| iz.blank(itmE.value))
+        {
+            gtg = false;
+            itmE.value = '';
+            itmE.placeholder = 'Invalid Email!';
+            itmE.style.backgroundColor = 'red';  
+        }
+        if(gtg)
+        {
+            //continue submitting form
+            
+        }
   }
   handleClearForm() {
-    // clear form logic goes here
+    var itmN = document.getElementById('frmName');
+    var itmP = document.getElementById('frmPhone');
+    var itmW = document.getElementById('frmWeb');
+    var itmE = document.getElementById('frmEmail');
+    itmN.value = '';
+    itmP.value = '';
+    itmW.value = '';
+    itmE.value = '';
+    itmN.style.backgroundColor = 'white';
+    itmP.style.backgroundColor = 'white';
+    itmW.style.backgroundColor = 'white';
+    itmE.style.backgroundColor = 'white';
+    itmN.placeholder = 'Type first and last name here';
+    itmP.placeholder = 'Enter your phone number';
+    itmW.placeholder = 'Enter your website';
+    itmE.placeholder = 'Enter your Email Address';
   }
 
 
@@ -53,35 +132,53 @@ class FormContainer extends Component {
       <form className="container" onSubmit={this.handleFormSubmit}>
         <h5>Tell me about yourself!</h5>
         <div className="itmForm">
-        <input
-          type='text'
-          id='frmName'
-          name={'name'}
-          onChange={this.handleFullNameChange}
-          content="hh"
-          placeholder={'Type first and last name here'} />
+            <input
+            type='text'
+            className='tbox'
+            id='frmName'
+            name={'name'}
+            onChange={this.handleFullNameChange}
+            placeholder={'Type first and last name here'} />
         </div>
-                <div className="itmForm">
-        <input
-          type='text'
-          id='frmPhone'
-          name={'name'}
-          onChange={this.handlePhoneChange}
-          content="hh"
-          placeholder={'Enter your phone number'}
-          ValidChars="1234567890"  />
+        <div className="itmForm">
+            <input
+            type='text'
+            className='tbox'
+            id='frmPhone'
+            name={'phone'}
+            onChange={this.handlePhoneChange}
+            placeholder={'Enter your phone number'} />
+        </div>
+        <div className="itmForm">
+            <input
+            type='text'
+            className='tbox'
+            id='frmWeb'
+            onChange={this.handleWebChange}
+            name={'web'}
+            placeholder={'Enter your website'} />
+        </div>
+        <div className="itmForm">
+            <input
+            type='text'
+            className='tbox'
+            id='frmEmail'
+            onChange={this.handleEmailChange}
+            name={'email'}
+            placeholder={'Enter your Email Address'}  />
         </div>
         <div className="btnForm">
-            <button
+            <input
+            type='button'
             className="btnFormBtn"
-            onClick={this.handleFormSubmit}>
-                Submit
-            </button>
-            <button
+            onClick={this.handleFormSubmit} 
+            value='Submit'/>
+            <input
+            type='button'
             className="btnFormBtn"
-            onClick={this.handleClearForm}>
-                Clear form
-            </button>
+            onClick={this.handleClearForm} 
+            value='Clear'/>
+
         </div>
       </form>
   );
